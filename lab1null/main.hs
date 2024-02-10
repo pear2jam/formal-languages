e360c326cd1bd6760c4eb4fe9c2024357902b9cc
pear2jam <71353007+pear2jam@users.noreply.github.com>
2024-02-10 16:01:33 +0300

    Update main.hs

--- lab1null/main.hs
+++ lab1null/main.hs
@@ -40,6 +40,13 @@ main = do
 
   res' ""
 
+  forM_ [0 .. length a - 1] $ \i ->
+    forM_ [0 .. length a - 1] $ \j -> do
+      res' $ "(declare-const MMd" ++ show i ++ "d" ++ show j ++ " Int)"
+      res' $ "(assert (>= MMd" ++ show i ++ "d" ++ show j ++ " 0))"
+
+  res' ""
+
   -- определим последнюю доминошку
   forM_ [0 .. length a - 1] $ \i -> do
     res' $ "(declare-const IsLast_d" ++ show i ++ " Int)"
@@ -51,10 +58,30 @@ main = do
 
   res' ""
 
+  -- определим первую доминошку (!)
+  forM_ [0 .. length a - 1] $ \i -> do
+    res' $ "(declare-const IsFirst_d" ++ show i ++ " Int)"
+    res' $ "(assert (>= IsFirst_d" ++ show i ++ " 0))"
+
+  let sum_firsts = unwords ["IsFirst_d" ++ show i | i <- [0 .. length a - 1]]
+
+  res' $ "(assert (= (+ " ++ sum_firsts ++ ") 1))"
+
+  res' ""
+
   -- связываем количество доминошек и пар доминошек
+  -- последние
   forM_ [0 .. length a - 1] $ \i -> do
     let sum_d = unwords ["Md" ++ show i ++ "d" ++ show j | j <- [0 .. length a - 1]]
     res' $ "(assert(=(+ " ++ sum_d ++ ")(- Md" ++ show i ++ " IsLast_d" ++ show i ++ ")))"
+
+  res' ""
+
+  -- первые (!)
+
+  forM_ [0 .. length a - 1] $ \i -> do
+    let sum_d = unwords ["MMd" ++ show i ++ "d" ++ show j | j <- [0 .. length a - 1]]
+    res' $ "(assert(=(+ " ++ sum_d ++ ")(- Md" ++ show i ++ " IsFirst_d" ++ show i ++ ")))"
 
   res' ""
 
@@ -95,6 +122,20 @@ main = do
 
   res' ""
 
+  -- !!!
+
+  forM_ [0 .. length a - 1] $ \i ->
+    forM_ [0 .. length a - 1] $ \j ->
+      forM_ alphabet $ \letter1 ->
+        forM_ alphabet $ \letter2 -> do
+          res' $ "(declare-const PPu_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ "d" ++ show j ++ " Int)"
+          res' $ "(assert (= PPu_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ "d" ++ show j ++ " " ++ show (cnt' ((last (a !! j !! 0) : []) ++ (head (a !! i !! 0) : [])) [letter1, letter2]) ++ "))"
+
+          res' $ "(declare-const PPd_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ "d" ++ show j ++ " Int)"
+          res' $ "(assert (= PPd_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ "d" ++ show j ++ " " ++ show (cnt' ((last (a !! j !! 1) : []) ++ (head (a !! i !! 1) : [])) [letter1, letter2]) ++ "))"
+
+  res' ""
+
   -- сравним количество букв
   forM_ alphabet $ \letter -> do
     let sum_u = unwords ["(* Md" ++ show i ++ " Lu_" ++ [letter] ++ "d" ++ show i ++ ")" | i <- [0 .. length a - 1]]
@@ -104,7 +145,6 @@ main = do
   res' ""
 
   -- сравним количество пар букв
-  -- измененный подсчет
 
   forM_ alphabet $ \letter1 ->
     forM_ alphabet $ \letter2 -> do
@@ -115,6 +155,18 @@ main = do
               ++ ") (+ "
               ++ concatMap (\i -> "(* Md" ++ show i ++ " Pd_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ ")") [0 .. length a - 1]
               ++ concatMap (\i -> concatMap (\j -> "(* Md" ++ show i ++ "d" ++ show j ++ " Pd_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ "d" ++ show j ++ ")") [0 .. length a - 1]) [0 .. length a - 1]
+              ++ ")))"
+      res' r
+
+  forM_ alphabet $ \letter1 ->
+    forM_ alphabet $ \letter2 -> do
+      let r =
+            "(assert (= (+"
+              ++ concatMap (\i -> "(* Md" ++ show i ++ " Pu_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ ")") [0 .. length a - 1]
+              ++ concatMap (\i -> concatMap (\j -> "(* MMd" ++ show i ++ "d" ++ show j ++ " PPu_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ "d" ++ show j ++ ")") [0 .. length a - 1]) [0 .. length a - 1]
+              ++ ") (+ "
+              ++ concatMap (\i -> "(* Md" ++ show i ++ " Pd_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ ")") [0 .. length a - 1]
+              ++ concatMap (\i -> concatMap (\j -> "(* MMd" ++ show i ++ "d" ++ show j ++ " PPd_" ++ [letter1] ++ [letter2] ++ "d" ++ show i ++ "d" ++ show j ++ ")") [0 .. length a - 1]) [0 .. length a - 1]
               ++ ")))"
       res' r
 
